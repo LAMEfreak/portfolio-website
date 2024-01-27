4cb34c1701f4975041b7998e012900ad83951f40
LAMEfreak <leeweirong@outlook.com>
2024-01-28 06:16:57 +0800

    Completed portfolio website

---
+++ src/App.jsx
@@ -0,0 +1,104 @@
+import "./App.css";
+import { useEffect, useRef } from "react";
+
+import chefVideo from "./assets/recipe.mp4";
+import clockVideo from "./assets/clock.mp4";
+import OtherProject from "./components/OtherProject";
+import Hero from "./components/Hero";
+import Highlight from "./components/Highlight";
+import music from "./assets/Hymn for the Weekend.mp3";
+
+function App() {
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current.volume = 0.25;
+    const updateBackground = (e) => {
+      document.body.style.setProperty("--x", `${e.pageX}px`);
+      document.body.style.setProperty("--y", `${e.pageY}px`);
+    };
+
+    window.addEventListener("mousemove", updateBackground);
+
+    return () => {
+      window.removeEventListener("mousemove", updateBackground);
+    };
+  }, []);
+
+  return (
+    <>
+      <div className="fixed inset-x-0 flex flex-col pt-6 items-center opacity-10 hover:opacity-90 transition:all duration-700 w-full z-20">
+        <audio ref={audioRef} src={music} controls></audio>
+        <p className="mt-3 font-semibold">Coldplay ➖ Hymn for the Weekend</p>
+      </div>
+      <Hero
+        h1="Weirong's Portfolio"
+        h2="Weaving Wonders with Web Dev"
+        description="I build delightful digital product experiences that inspire users and empower businesses. Learning full-stack development at Rocket Academy."
+      />
+      <div>
+        <h2 className="text-5xl font-semibold text-center">Highlights</h2>
+        <div className="w-[16rem] border-solid border border-gray-600 mt-8 rounded-md mb-[8rem] mx-auto"></div>
+
+        <Highlight
+          headerColor="text-cyan-300"
+          bgColor="bg-[#0d253f]"
+          textColor="text-[#a9b6e2]"
+          bgColorPill="bg-[#153c65]"
+          textColorPill="text-cyan-200"
+          title="Recipe Site"
+          description="This website showcases 4 unique dishes across Chinese and Japanese cuisines. Each dish is beautifully presented with detailed recipes and step-by-step instructions. Explore the flavors of Asia and learn how to recreate these delicious dishes in your own kitchen."
+          technologies={["HTML", "CSS", "JavaScript"]}
+          video={chefVideo}
+          link="https://lamefreak.github.io/recipe-site-bootcamp/"
+        />
+
+        <Highlight
+          headerColor="text-[#52edf5]"
+          bgColor="bg-[#0b3e44]"
+          textColor="text-[#75ebf8]"
+          bgColorPill="bg-[#166a6e]"
+          textColorPill="text-cyan-200"
+          title="World Clock"
+          description="The World Clock is a dynamic application that displays the current time in major cities across the world. It allows you to add and customize timezones, providing you with a convenient way to keep track of time differences and plan your activities accordingly."
+          technologies={["React", "React Bootstrap"]}
+          video={clockVideo}
+          link="https://lamefreak.github.io/world-clock-3.2/"
+        />
+      </div>
+      <h2 className="text-4xl font-semibold text-center mt-[20rem] text-cyan-300 mb-16">
+        Other Projects
+      </h2>
+      <div className="grid grid-cols-1 lg:grid-cols-3 gap-12 lg:gap-4 grid-flow-row">
+        <OtherProject
+          title="Streamlining F&B Order Workflow"
+          text="User research and high-fidelity mockups for a B2B message-to-order platform that connects small-to-medium F&B establishments with suppliers."
+          link="https://weironglee.webflow.io/foodtech-mobile-app"
+          type="Product Design"
+        />
+        <OtherProject
+          title="Launching Hotel Micro Stays"
+          text="Part of EntryLevel's Product Management program. Strategized the launch of hotel micro stays for Booking.com, which allows users to book hotels by the hour."
+          link="https://weironglee.webflow.io/booking-com-case-study"
+          type="Product Strategy"
+        />
+        <OtherProject
+          title="Webpage redesign for IVA"
+          text="Refreshed Insignia Ventures Academy's website to launch a new on-demand course. Communicated value proposition to founders and professionals interested in VC."
+          link="https://weironglee.webflow.io/iva-website"
+          type="Web Design"
+        />
+      </div>
+      <div className="flex flex-col gap-1 items-center mt-[8rem] pb-12">
+        <p className="text-sm">
+          Built by Weirong using <span className="font-bold">React</span>,{" "}
+          <span className="font-bold">TailwindCSS</span>,{" "}
+          <span className="font-bold">Vite</span>
+        </p>
+        <p className="text-sm">{`Inspired by Brittany Chiang's portfolio site`}</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/Highlight.jsx
@@ -0,0 +1,74 @@
+import PropTypes from "prop-types";
+
+const Highlight = ({
+  headerColor,
+  bgColor,
+  textColor,
+  bgColorPill,
+  textColorPill,
+  title,
+  description,
+  technologies,
+  video,
+  link,
+}) => {
+  return (
+    <div className="mb-20 xl:mb-60">
+      <a href={link}>
+        <div className="relative z-10">
+          <h3
+            className={`${headerColor} font-semibold text-4xl mb-8 text-center sm:text-left`}
+          >
+            {title}
+          </h3>
+          <div
+            className={`w-[100%] xl:w-[55%] ${bgColor} p-8 leading-7 ${textColor} rounded-lg shadow-2xl`}
+          >
+            {description}
+            <div className="flex gap-3 flex-wrap mt-6">
+              {technologies.map((technology) => {
+                return (
+                  <div
+                    key={technology}
+                    className={`${bgColorPill} ${textColorPill} px-4 rounded-2xl text-[14px] font-semibold`}
+                  >
+                    {technology}
+                  </div>
+                );
+              })}
+            </div>
+
+            <a href={link}>
+              <div className="absolute hidden xl:block mb-4 -top-0 left-2/4 z-[-2] ">
+                <video
+                  src={video}
+                  type="video/mp4"
+                  width="800"
+                  className="rounded-2xl opacity-70 hover:shadow-2xl hover:opacity-100 transition duration-300"
+                  autoPlay
+                  loop
+                  muted
+                ></video>
+              </div>
+            </a>
+          </div>
+        </div>
+      </a>
+    </div>
+  );
+};
+
+Highlight.propTypes = {
+  headerColor: PropTypes.string.isRequired,
+  bgColor: PropTypes.string.isRequired,
+  textColor: PropTypes.string.isRequired,
+  bgColorPill: PropTypes.string.isRequired,
+  textColorPill: PropTypes.string.isRequired,
+  title: PropTypes.string.isRequired,
+  description: PropTypes.string.isRequired,
+  technologies: PropTypes.string.isRequired,
+  video: PropTypes.string.isRequired,
+  link: PropTypes.string.isRequired,
+};
+
+export default Highlight;
